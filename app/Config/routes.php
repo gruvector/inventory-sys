fca5fa91805701c4b3132bd13ff43a704c2a1ae1
gruvector <vector.gru316@gmail.com>
2015-07-17 01:51:41 +0000

    added routing information

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -24,11 +24,11 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
-	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
+	Router::connect('/', array('controller' => 'Dashboard', 'action' => 'index'));
 /**
  * ...and connect the rest of 'Pages' controller's URLs.
  */
-	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
+	Router::connect('/pages/*', array('controller' => 'Dashboard', 'action' => 'index'));
 
 /**
  * Load all plugin routes. See the CakePlugin documentation on
